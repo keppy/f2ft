bdf68ffe2bdff2a1573882ef5aac3a481e5086b5
James Dominguez <jdominguez@godaddy.com>
2015-08-25 11:26:56 -0700

    User login.

---
+++ app/routes/login.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import PouchDB from 'pouchdb';
+
+var remote = new PouchDB('http://localhost:5984/f2ft');
+
+export default Ember.Route.extend({
+  actions: {
+    submit() {
+      let username = this.controller.get('username');
+      let work = this.controller.get('work');
+
+      let promise = remote.login(username, work);
+
+      promise.then((res) => {
+      });
+    }
+  }
+});
--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,9 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    sassOptions: {
+      includePaths: ['bower_components/materialize/sass']
     }
   };
 
@@ -42,6 +45,11 @@ module.exports = function(environment) {
   if (environment === 'production') {
 
   }
+
+  ENV.contentSecurityPolicy = {
+    "connect-src": "'self' http://localhost:5984",
+    "style-src": "'self' 'unsafe-inline' http://localhost:5984"
+  };
 
   return ENV;
 };
